e1ee4087609b54081d17e3faeb9ea12d3cee9436
gnappo1 <gnappo.1@live.it>
2018-06-14 15:06:12 -0700

    Imported HostBinding to bind local property to HTML element property.

--- Development/code/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ Development/code/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,15 +1,28 @@
-import { Directive, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Directive, OnInit, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
+//another decorator can let us avoid using the renderer, HostBinding
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
+
 export class BetterHighlightDirective implements OnInit {
+
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    //takes the element to style, the style property to set, the value and, lastly, a flag (optional). 
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    //takes the element to style, the style property to set, the value and, lastly, a flag (optional).
   }
 
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue', false);
+    this.backgroundColor = 'blue'
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent', false);
+    this.backgroundColor = 'transparent';
+  }
 }
